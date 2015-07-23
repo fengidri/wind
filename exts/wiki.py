1d89f28635a149ebe82fb96c19814a69f71d2060
fengidri <fengidri@gmail.com>
2015-07-23 12:26:49 +0800

    complete wiki.py

--- exts/wiki.py
+++ exts/wiki.py
@@ -16,149 +16,143 @@ import requests
 
 import frainui
 
-ID = None
+
 SERVER="blog.fengidri.me"
-is_wiki = False
-
-map_id_file = {}
-map_file_leaf = {}
-
 
 TEXLIST = None
-class Cfg(object):
-    texlist = None
-    texinfo = None
-
-    @classmethod
-    def get_name_by_id(cls, ID):
-        for info in cls.texinfo:
-            if info[0] == ID:
-                return info[1]
-
-    def get_file_by_id(cls, ID):
-        pass
-
-
-################################################################################
-# paser content
-################################################################################
-def getv(line):
-    tmp = line[1:].split(':')
-    if len(tmp) < 2:
-        return []
-    return [tmp[0].strip().lower(), tmp[1].strip()]
-
-
-def content():
-    info = {}
-    for line in vim.current.buffer:
-        if len(line) < 2 or line[0] != '%':
-            if not info.get('title'):
-                print "not find title"
-                return
-            break
-        try:
-            k,v = getv(line)
-            info[k] = v
-        except:
-            pass
-
-    if not info.get('title'):
-        print "not find title"
-        return
-    content = '\n'.join(vim.current.buffer)
-    return (info, content)
 
 ################################################################################
 # 连接服务器
 ################################################################################
+class Remote(object):
+    def __new__(cls, *args, **kw):
+        if not hasattr(cls, '_instance'):
+            orig = super(Remote, cls)
+            cls._instance = orig.__new__(cls, *args, **kw)
+        return cls._instance
 
-def load_list():
-    URL = "http://blog.fengidri.me/store/index.json"
-    try:
-        response = urllib2.urlopen(URL)
-        info = response.read()
-        info = json.loads(info)
-        info.reverse()
-    except:
-        pyvim.echo("load index.json fail!")
-        info = []
-    Cfg.texinfo = info
-    return info
+    def __init__(self):
+        if hasattr(self, "map_id_tmp_file"):
+            return
+        self.map_id_tmp_file = {}
 
-def load_tex(ID):
-    tmp = map_id_file.get(ID)
-    if tmp:
+    def file_to_id(self, f):
+        for ID, _f in self.map_id_tmp_file.items():
+            if _f == f:
+                return ID
+
+    def load_list(self):
+        URL = "http://blog.fengidri.me/store/index.json"
+        try:
+            response = urllib2.urlopen(URL)
+            info = response.read()
+            info = json.loads(info)
+        except:
+            pyvim.echo("load index.json fail!")
+            info = {}
+        self.info = info
+
+    def iter(self):
+        keys = [int(k) for k in self.info.keys()]
+        keys.sort()
+        keys.reverse()
+        for k in keys:
+            yield k, self.info[str(k)]
+
+
+    def load_tex(self, ID):
+        tmp = self.map_id_tmp_file.get(ID)
+        if tmp:
+            return tmp
+
+        url = 'http://%s/store/%s/index.mkiv' % (SERVER, ID)
+        req = urllib2.Request(url)
+        tmp = tempfile.mktemp(suffix='.mkiv', prefix='fwiki_%s_' % ID)
+        try:
+            res = urllib2.urlopen(req).read()
+        except Exception, e:
+            pyvim.echo(e)
+            return
+        open(tmp, 'wb').write(res)
+        self.map_id_tmp_file[ID] = tmp
         return tmp
 
-    url = 'http://%s/store/%s/index.mkiv' % (SERVER, ID)
-    req = urllib2.Request(url)
-    tmp = tempfile.mktemp(suffix='.mkiv', prefix='fwiki_%s_' % ID)
-    try:
-        res = urllib2.urlopen(req).read()
-    except Exception, e:
-        pyvim.echo(e)
-        return
-    open(tmp, 'wb').write(res)
-    map_id_file[ID] = tmp
-    return tmp
+    def post_tex(self, tex, ID):
+        j = { 'tex': tex, 'html': html(buf = tex) }
+        if ID:
+            method = "PUT"
+            uri = 'http://%s/fwikiapi/chapters/%s' % (SERVER, ID)
+        else:
+            method = "POST"
+            uri = 'http://%s/fwikiapi/chapters' % SERVER
 
-def post_tex(tex, info, ID):
-    j = {
-            'title':info.get('title'),
-            'tex': tex,
-            'html': html(buf = tex),
-            'class': info.get('class', ''),
-            'post': info.get('post', 'true')
-            }
-    if ID:
-        method = "PUT"
-        uri = 'http://%s/fwikiapi/chapters/%s' % (SERVER, ID)
-    else:
-        method = "POST"
-        uri = 'http://%s/fwikiapi/chapters' % SERVER
+        headers = {'Content-Type': "application/json"};
 
-    headers = {'Content-Type': "application/json"};
+        res = requests.request(method, uri, headers=headers, data=json.dumps(j))
+        return int(res.text)
 
-    res = requests.request(method, uri, headers=headers, data=json.dumps(j))
-    return int(res.text)
+
+
+
+
 
 
 ################################################################################
 # 处理 frainui
 ################################################################################
 
-def leaf_handle(leaf):
-    if -1 == leaf.ctx: # new wiki
-        tmp = tempfile.mktemp(suffix='.mkiv', prefix='fwiki_')
-        vim.command('e %s' % tmp)
-        vim.current.buffer.append('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%', 0)
-        vim.current.buffer.append('%Post:1', 0)
-        vim.current.buffer.append('%Class:', 0)
-        vim.current.buffer.append('%Title:', 0)
-        vim.current.buffer.append('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%', 0)
-        return
+def add_new(node):
+    tmp = tempfile.mktemp(suffix='.mkiv', prefix='fwiki_')
+    vim.command('e %s' % tmp)
+    vim.current.buffer.append('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%', 0)
+    vim.current.buffer.append('%Post:1', 0)
+    vim.current.buffer.append('%Class:', 0)
+    vim.current.buffer.append('%Title:', 0)
+    vim.current.buffer.append('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%', 0)
+    return
 
-    tmp = load_tex(leaf.ctx)
+def leaf_handle(leaf):
+    remote = Remote()
+    tmp = remote.load_tex(leaf.ctx)
     if not tmp:
         return
-    map_file_leaf[tmp] = (leaf.ctx, leaf.name)
     vim.command("edit %s" % tmp)
 
-def GetRoots(node):
-    leaf = frainui.Leaf("new wiki", -1, leaf_handle)
-    node.append(leaf)
-    for info in load_list():
-        name = info[1]
-        ID = info[0]
-        leaf = frainui.Leaf(name, ID, leaf_handle)
-        node.append(leaf)
+def list_tex(node):
+    remote = Remote()
+    remote.load_list()
+    for ID, info in remote.iter():
+        if node.ctx == "TexList" and info.get("post", '1') != '0':
+            name = info.get("title", 'undefined')
+            leaf = frainui.Leaf(name, int(ID), leaf_handle)
+            node.append(leaf)
 
-#def GetNames(listwin):
-#    ID, name = map_file_leaf.get(vim.current.buffer.name)
-#    if not leaf:
-#        return
-#    listwin.setnames([name])
+        if node.ctx == "UnPost" and info.get("post", '1') == '0':
+            name = info.get("title", 'undefined')
+            leaf = frainui.Leaf(name, int(ID), leaf_handle)
+            node.append(leaf)
+
+def GetRoots(node):
+    leaf = frainui.Leaf("NewWiki", -1, add_new)
+    node.append(leaf)
+
+    n = frainui.Node("TexList", "TexList", list_tex)
+    node.append(n)
+
+    n = frainui.Node("UnPost", "UnPost", list_tex)
+    node.append(n)
+
+
+def GetNames(listwin):
+    remote = Remote()
+    ID = remote.file_to_id(vim.current.buffer.name)
+    info = remote.info.get(str(ID))
+    if info.get('post', '1') == '0':
+        names = ['UnPost', info.get('title')]
+    else:
+        names = ['TexList', info.get('title')]
+    logging.error(names)
+    listwin.setnames(names)
 
 
 def ReFreshPre(listwin):
@@ -168,12 +162,12 @@ def ReFreshPre(listwin):
 ################################################################################
 # command && event
 ################################################################################
-
 @pyvim.cmd()
 def TexList():
     global  TEXLIST
     TEXLIST = frainui.LIST(GetRoots)
-    #TEXLIST.FREventBind("ListNames", GetNames)
+
+    TEXLIST.FREventBind("ListNames", GetNames)
     TEXLIST.FREventBind("ListReFreshPre", ReFreshPre)
     TEXLIST.show()
     TEXLIST.refresh()
@@ -184,19 +178,17 @@ def TexList():
 def WikiPost():
     if not TEXLIST: return
 
-    ID = None
-    info, c = content()
-    tt = map_file_leaf.get(vim.current.buffer.name)
+    remote = Remote()
 
-    if tt:
-        need_fresh = False
-        ID = tt[0]
+    ID = remote.file_to_id(vim.current.buffer.name)
+    ID = remote.post_tex('\n'.join(vim.current.buffer), ID)
 
-    _ID = post_tex(c, info, ID)
+    if ID < 0:
+        pyvim.echo("POST error: %d" % ID)
+        return
 
-    if ID == None:
-        map_id_file[_ID] = vim.current.buffer.name
-        TEXLIST.refresh()
+    remote.map_id_tmp_file[ID] = vim.current.buffer.name
+    TEXLIST.refresh()
 
 @pyvim.event("CursorHold", "*.mkiv")
 @pyvim.event("CursorHoldI", "*.mkiv")
