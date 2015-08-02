70b4c93f449a057a6839da8585e9e7a605a806ea
fengidri <fengidri@gmail.com>
2015-08-02 13:48:48 +0800

    fix plugins

--- wind/plugins.py
+++ wind/plugins.py
@@ -7,6 +7,7 @@
 # TODO 试图导入目录. 可以使用修改 __builtin__.__import__ 的方法
 
 import os
+import sys
 import imp
 from pyvim import log as logging
 import pyvim
@@ -21,25 +22,33 @@ class Plugins(object):
             logging.error("plugin path:[%s] is not dir"%self.path)
             return
 
-        modules = os.listdir(self.path)
-
-        for module in modules:
-            if not module.endswith(".py"):
-                continue
-
-            module_name = module[0:-3]
-
+        for module in os.listdir(self.path):
             module_path = os.path.join(self.path, module)
-            sys_name = module_path.replace('.', '_')
+            if os.path.isdir(module_path):
+                _module_path = os.path.join(module_path, '__init__.py')
+                if not os.path.exists(_module_path):
+                    continue
+                sys.path.insert(0, module_path)
+                self.load_source(_module_path)
+            else:
+                self.load_source(module_path)
 
-            try:
-                module_loaded = imp.load_source(sys_name, module_path)
-            except:
-                pyvim.echo("Load [%s] Fail"% module_name)
-                import traceback
-                logging.error(traceback.format_exc())
-            if self.hook_init:
-                self.hook_init(module_name, module_loaded)
+
+    def load_source(self, path):
+        if not path.endswith(".py"):
+            return
+
+        sys_name = path.replace('.', '_')
+
+        try:
+            module_loaded = imp.load_source(sys_name, path)
+        except:
+            pyvim.echo("Load [%s] Fail"% path)
+            import traceback
+            logging.error(traceback.format_exc())
+
+        #if self.hook_init:
+        #    self.hook_init(module[0:-3], module_loaded)
 
 
 if __name__ == "__main__":
