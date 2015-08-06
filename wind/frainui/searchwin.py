a55bd3c839240c955fe77d9f8c901f1df5e0cf8d
fengidri <fengidri@gmail.com>
2015-08-06 15:02:37 +0800

    searchwin delete pat by -

--- wind/frainui/searchwin.py
+++ wind/frainui/searchwin.py
@@ -8,20 +8,26 @@ import pyvim
 import utils
 import vim
 
-def match_lines(pats, lines, mx = None):
+def match_lines(pats, ng_pats, lines, mx = None):
+    pyvim.log.error("%s %s", pats, ng_pats)
     tmp = []
     index = 0
     if mx == None:
         mx = len(lines)
+
     for line in lines:
-        for pat in pats:
-            if not pat in line:
+        for pat in ng_pats:
+            if pat in line:
                 break
         else:
-            index += 1
-            if index >= mx:
-                return
-            tmp.append(line)
+            for pat in pats:
+                if not pat in line:
+                    break
+            else:
+                index += 1
+                if index >= mx:
+                    return tmp
+                tmp.append(line)
     return tmp
 
 
@@ -54,14 +60,24 @@ class SearchWIN(utils.Object):
         if c.find(';') > -1:
             return
 
-        pats = c.split()
-        lines = match_lines(pats, self.lines, 20)
-        del self.buf.b[1:]
+        pats = []
+        ng_pats = []
+        for pat in c.split():
+            if pat[0] == '-':
+                pat = pat[1:]
+                if pat:
+                    ng_pats.append(pat[1:])
+            else:
+                pats.append(pat)
 
+        lines = match_lines(pats, ng_pats, self.lines, 20)
         self.show_list(lines)
         self.hi_pats(pats)
 
     def show_list(self, lines, num=15):
+        del self.buf.b[1:]
+        if not lines:
+            return
         num = min(len(lines), num)
         for i in range(0, num):
             line = lines[i]
