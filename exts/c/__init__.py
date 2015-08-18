6b7c601005cfa50b1bc07a810bda96b30607304c
fengidri <fengidri@gmail.com>
2015-08-18 09:14:26 +0800

    sc

---
+++ exts/c/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+#    author    :   丁雪峰
+#    time      :   2015-08-18 09:13:23
+#    email     :   fengidri@yeah.net
+#    version   :   1.0.1
+
+import c
+import goto_inc
+
+if __name__ == "__main__":
+    pass
+
