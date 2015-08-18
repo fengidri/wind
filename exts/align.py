15ba9eeeb839ed300956542fc06e600d8d1ac2a2
fengidri <fengidri@gmail.com>
2015-08-18 10:12:25 +0800

    sc

--- exts/align.py
+++ exts/align.py
@@ -7,37 +7,9 @@
 import pyvim
 import vim
 
-@pyvim.cmd()
-def Align(tag = ' '):
-        pos1, pos2 = pyvim.selectpos()
-        line1 = pos1[0]
-        line2 = pos2[0] + 1
-        lines = vim.current.buffer[line1: line2]
-
-        if tag == ' ':
-            lines = align_fun(lines)
-        else:
-            lines = align_fun_with_tag(lines, tag)
-
-        vim.current.buffer[line1: line2] = lines
-
-def align_fun_with_tag(lines, tag):
-    lines = [line.split(tag) for line in lines]
-
-    lines = align(lines)
-
-    join_tag = " " + tag + ' '
-    return [join_tag.join(line).rstrip() for line in lines]
-
-def align_fun(lines, tag = ' '):
-    lines = [align_split(line) for line in lines]
-
-    lines = align(lines)
-
-    return [' '.join(line).rstrip() for line in lines]
-
 def align(lines):
     max_len = 0
+
     for line in lines:
         max_len = max(max_len, len(line))
 
@@ -45,12 +17,14 @@ def align(lines):
     for line in lines:
         for i, w in enumerate(line):
             max_len_list[i] = max(max_len_list[i], len(w))
+
     for line in lines:
         for i, w in enumerate(line):
             if i == 0:
                 line[i] = w.rstrip().ljust(max_len_list[i])
             else:
                 line[i] = w.strip().ljust(max_len_list[i])
+
     return lines
 
 
@@ -75,6 +49,35 @@ def align_split(line):
         split_list.append(''.join(buf))
         del buf[:]
     return split_list
+
+def align_fun_with_tag(lines, tag):
+    lines = [line.split(tag) for line in lines]
+
+    lines = align(lines)
+
+    join_tag = " " + tag + ' '
+    return [join_tag.join(line).rstrip() for line in lines]
+
+def align_fun(lines, tag = ' '):
+    lines = [align_split(line) for line in lines]
+
+    lines = align(lines)
+
+    return [' '.join(line).rstrip() for line in lines]
+
+@pyvim.cmd()
+def Align(tag = ' '):
+        pos1, pos2 = pyvim.selectpos()
+        line1 = pos1[0]
+        line2 = pos2[0] + 1
+        lines = vim.current.buffer[line1: line2]
+
+        if tag == ' ':
+            lines = align_fun(lines)
+        else:
+            lines = align_fun_with_tag(lines, tag)
+
+        vim.current.buffer[line1: line2] = lines
 
 
 
